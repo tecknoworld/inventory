7adc492027352b3f7217aed762742c38b7333cf7
tecknoworld <ofentselebogo@gmail.com>
2024-05-15 16:01:30 +0200

    reactprac

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+   <Info/>
     </div>
   );
+
+
+
+  function Info() {
+    return (
+      <div>
+    <h1>Inventory management system</h1>
+      <p>Making storing stuff easy</p>
+      </div>
+  
+    );
+  }
 }
 
 export default App;
